9a8c12451a63e8bc13f9009203c5a857b36c23aa
Faith Dele Alonge <126901421+sloetag@users.noreply.github.com>
2026-06-22 15:18:17 +0100

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -27,7 +27,7 @@ const projects = [
     tech: ['React', 'Zustand', 'Framer Motion', 'Radix UI'],
     image: 'https://images.unsplash.com/photo-1595301490405-0ae747be39f6?q=80&w=1150&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     color: 'from-orange-500/20 to-red-500/20',
-    liveUrl: 'https://sloethedev.vercel.app',
+    liveUrl: 'https://wooltown.vercel.app',
   }
 ];
 
